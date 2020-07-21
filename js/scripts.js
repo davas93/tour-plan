456067edad1ca205d3d426ff0fd0de31fa87ae8f
davas93 <vasin.da93@gmail.com>
2020-07-21 21:05:01 +0300

    create: add ymaps API

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,39 @@ var mySwiper = new Swiper(".swiper-container", {
     onlyInViewport: false,
   },
 });
+//Подключение API Яндекс Карты
+ymaps.ready(function () {
+  var myMap = new ymaps.Map(
+      "map",
+      {
+        center: [12.934948, 100.88328],
+        zoom: 15.3,
+      },
+      {
+        searchControlProvider: "yandex#search",
+      }
+    ),
+    // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
+    myPlacemark = new ymaps.Placemark(
+      myMap.getCenter(),
+      {
+        hintContent: "Grand Hilton Hotel",
+      },
+      {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: "default#image",
+        // Своё изображение иконки метки.
+        iconImageHref: "../img/hotel/location-icon.svg",
+        // Размеры метки.
+        iconImageSize: [40, 52],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-25, -40],
+      }
+    );
+  myMap.geoObjects.add(myPlacemark);
+});
