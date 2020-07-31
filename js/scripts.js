9a6070615a1417e477b2672054e7e9912b941351
davas93 <vasin.da93@gmail.com>
2020-07-31 19:24:40 +0300

    create: modal window

--- js/scripts.js
+++ js/scripts.js
@@ -97,3 +97,22 @@ $(".menu-button").on("click", function () {
 $(".navbar-menu__item").on("click", function () {
   $(".navbar-menu").removeClass("navbar-menu--active");
 });
+
+//Модальное окно
+var modalButton = $("[data-toggle=modal]");
+var closeModalButton = $(".modal__close");
+modalButton.on("click", openModal);
+closeModalButton.on("click", closeModal);
+
+function openModal() {
+  var targetModal = $(this).attr("data-href");
+  $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+  $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+}
+function closeModal(event) {
+  event.preventDefault();
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass("modal__overlay--visible");
+  modalDialog.removeClass("modal__dialog--visible");
+}
